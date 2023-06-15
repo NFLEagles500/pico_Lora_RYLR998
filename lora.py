eb19c7645cd77caec0276e3fdbbb984c402264d6
NFLEagles500 <NFLEagles500@gmail.com>
2023-06-14 21:16:08 -0600

    Update lora.py
    added some checks to determine if a full reset of the lora module is needed.

--- lora.py
+++ lora.py
@@ -10,7 +10,16 @@ from machine import Pin, UART
 from time import sleep_ms, sleep
 import secrets
 
-clientAddr = 1306
+clientAddr = int() #Each client on your Lora network must have a unique address.
+#Set the following if you plan to send messages to another client.
+#Note that if you set the number 0, anything you send is broadcast to all members.
+recieverAddr = int()
+
+#optionally you can change parameters.  By default it chooses 9,7,1,12.
+# 8,7,1,12 works for my current setup
+parameters = '8,7,1,12'
+
+
 #Check for Pico or Pico W to set led pin (pico=25,picow='LED')
 import os
 devCheck = os.uname()
@@ -18,6 +27,8 @@ if 'Pico W' in devCheck.machine:
     led = Pin('LED', Pin.OUT)
 else:
     led = Pin(25, Pin.OUT)
+
+#LED off while initializing Lora
 led.value(0)
 baud = 115200
 class RYLR998:
@@ -36,11 +47,11 @@ class RYLR998:
         while True:
             try:
                 self._uart.write('{}\r\n'.format(lora_cmd))
-                sleep(2)
                 while(self._uart.any()==0):
                     pass
+                sleep(2)
                 reply = self._uart.read()
-                sleep(1)
+                sleep(0.2)
                 if retrn:
                     return reply.decode().strip('\r\n')
                 print(reply.decode().strip('\r\n'))
@@ -48,50 +59,56 @@ class RYLR998:
             except UnicodeError:
                 pass
             
-    def test(self):
+    def test(self,retrn=False):
         self._uart.write('AT\r\n') #was 'ATrn'
-        while(self._uart.any()==0):
+        while(self._uart.any()<=2):
             pass
+        sleep(0.5)
         reply = self._uart.readline()
+        if retrn:
+            return reply.decode()
         print(reply.decode())#.strip('rn'))
 
-    def set_addr(self, addr):
+    def set_addr(self, addr, retrn=False):
         self._uart.write('AT+ADDRESS={}\r\n'.format(addr))
-        while(self._uart.any()==0):
-            pass
-        reply = self._uart.readline()
-        print('Address set to: {}'.format(addr))
+        if retrn:
+            while(self._uart.any()<=2):
+                pass
+            reply = self._uart.read()
+            sleep(0.5)
+            return reply.decode().strip('\r\n')
         
     def set_pswd(self, cpin, retrn=False):
         self._uart.write('AT+CPIN={}\r\n'.format(cpin))
-        sleep(2)
-        while(self._uart.any()==0):
-            pass
-        reply = self._uart.read()
-        sleep(0.5)
         if retrn:
+            while(self._uart.any()<=2):
+                pass
+            reply = self._uart.read()
+            sleep(0.5)
             return reply.decode().strip('\r\n')
-        #print(reply.decode().strip('\r\n'))
 
     def set_networkid(self, netId, retrn=False):
         self._uart.write('AT+NETWORKID={}\r\n'.format(netId))
-        sleep(2)
-        while(self._uart.any()==0):
-            pass
-        reply = self._uart.read()
-        sleep(0.5)
         if retrn:
+            while(self._uart.any()<=2):
+                pass
+            reply = self._uart.read()
+            sleep(0.5)
             return reply.decode().strip('\r\n')
-        #print(reply.decode().strip('\r\n'))
 
-    def send_msg(self, addr, msg): #max message size: 240 bytes
-        print(f"Attempting to send {addr} this message: {msg}")
+    def send_msg(self, addr, msg, retrn=False): #max message size: 240 bytes
+        #print(f"Attempting to send {addr} this message: {msg}")
         self._uart.write('AT+SEND={},{},{}\r\n'.format(addr,len(msg),msg))
-        while(self._uart.any()==0):
-            pass
-        sleep(2)
-        reply = self._uart.readline()
-        print(f"send_msg: {reply.decode().strip('\r\n')}")
+        #If you want the response
+        if retrn:
+            while(self._uart.any()<=2):
+                pass
+            reply = self._uart.readline()
+            sleep(0.2)
+            try:
+                return reply.decode().strip('\r\n')
+            except UnicodeError:
+                return 'UnicodeError'
 
     def read_msg(self):
         if self._uart.any()==0:
@@ -99,7 +116,10 @@ class RYLR998:
         else:
             msg = ''
             while(self._uart.any()):
-                msg = msg + self._uart.read(self._uart.any()).decode()
+                try:
+                    msg = msg + self._uart.read(self._uart.any()).decode()
+                except UnicodeError:
+                    pass
             return msg.strip('\r\n')
 
 
@@ -108,27 +128,51 @@ lora = RYLR998(tx_pin=12,rx_pin=13) # Sets the UART port to be used. Defaults to
                 #and rx Pin 5.  Optionally, you can assign the variables 'tx_pin' and 'rx_pin'
                 # with the GPIO values you want.  Example: RYLR998(tx_pin=12,rx_pin=13)
 sleep(2)
-#Initialize device by first doing a factory reset
-lora.cmd('AT+FACTORY')
-sleep(0.5)
-#Reset the module
-lora.cmd('AT+RESET')
-sleep(2)
-#Set parameters
-lora.cmd('AT+PARAMETER=8,7,1,12')
-sleep(2)
-#Optionally create a NetworkId for your group of transceivers
-if hasattr(secrets, 'lora_nid'):
-    lora.set_networkid(secrets.lora_nid)
-    sleep(2)
-#Optionally create a 8 char password for simple encryption (chars 0-9 and A-F only), don't lose power on either side if you set password
-if hasattr(secrets, 'lora_pswd'):
-    lora.set_pswd(secrets.lora_pswd)
-    sleep(2)
+def initializeLoraModule():
+    #Initialize device by first doing a factory reset
+    lora.cmd('AT+FACTORY')
+    sleep(0.5)
+    #Reset the module
+    lora.cmd('AT+RESET')
+    sleep(0.5)
+    #Set parameters
+    lora.cmd(f'AT+PARAMETER={parameters}')
+    sleep(0.5)
+    #Optionally create a NetworkId for your group of transceivers
+    if hasattr(secrets, 'lora_nid'):
+        lora.set_networkid(secrets.lora_nid)
+        sleep(0.2)
+    #Optionally create a 8 char password for simple encryption (chars 0-9 and A-F only), don't lose power on either side if you set password
+    if hasattr(secrets, 'lora_pswd'):
+        lora.set_pswd(secrets.lora_pswd)
+        sleep(0.2)
+    #Set unique client address
+    lora.set_addr(clientAddr) 
+    sleep(0.2)
 
-#Set unique client address
-lora.set_addr(clientAddr) 
-sleep(2)
+loraModuleReset = False
+moduleAddr = lora.cmd('AT+ADDRESS?',retrn=True).split('=')[1]
+moduleParams = lora.cmd('AT+PARAMETER?',retrn=True).split('=')[1]
+if str(clientAddr) not in str(moduleAddr):
+    print(f"{moduleAddr} not {clientAddr}")
+    loraModuleReset = True
+elif hasattr(secrets, 'lora_nid'):
+    moduleNetId = lora.cmd('AT+NETWORKID?',retrn=True).split('=')[1]
+    if str(secrets.lora_nid) not in str(moduleNetId):
+        print(f"{moduleNetId} not {secrets.lora_nid}")
+        loraModuleReset = True
+elif hasattr(secrets, 'lora_pswd'):
+    modulePswd = lora.cmd('AT+CPIN?',retrn=True).split('=')[1]
+    if str(secrets.lora_pswd) not in str(modulePswd):
+        print(f"{modulePswd} not {secrets.lora_pswd}")
+        loraModuleReset = True
+elif str(parameters) not in str(moduleParams):
+    print(f"{moduleParams} not {parameters}")
+    loraModuleReset = True
+
+if loraModuleReset:
+    initializeLoraModule()
+
 #If a console is connected, lets return all the lora attributes for quick debugging
 print(f"ClientId: {lora.cmd('AT+ADDRESS?',retrn=True).split('=')[1]}")
 print(f"Network Id: {lora.cmd('AT+NETWORKID?',retrn=True).split('=')[1]}")
@@ -154,7 +198,7 @@ led.toggle()
 iter = 1
 while True:
     msgToSend = f'Sending message number {iter}'
-    lora.send_msg(2,msgToSend)
+    lora.send_msg(recieverAddr,msgToSend)
     iter += 1
     sleep(1)
     test = lora.read_msg()
